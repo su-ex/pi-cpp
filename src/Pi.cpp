22a26719aacf8af8d3cacbe82a7648c9b4a5c6fc
su-ex <codeworks@supercable.onl>
2017-03-16 23:51:18 +0100

    Optimizations

--- src/Pi.cpp
+++ src/Pi.cpp
@@ -3,30 +3,31 @@
 #include <gmpxx.h>
 
 int main() {
-	mpq_class t(1);
-	mpq_class sum(1103);
-	mpz_class n(1);
+	int n(1);
 	mpz_class faku(1);
-	mpz_class pote(1);
+	mpz_class pote(9801);
 	mpz_class inte;
-	
+	mpz_class sum(1103);
+	mpq_class t(1);
+
 	while (true) {
 		//Thread 0:
-		std::cout << std::setprecision(100000000) << mpf_class((mpq_class(9801, 2)*t)/sum,100000000) << std::endl;
-		
+		std::cout << std::setprecision(100000000) << mpf_class(mpq_class(pote, 2*sum)*t, 100000000) << std::endl;
+
 		//Thread 1:
 			//Thread 1.1:
 			faku *= (4*n-3)*(4*n-2)*(4*n-1)*4;
 			faku /= n*n*n;
 			inte = (1103+26390*n++)*faku;
 			//Thread 1.2:
-			pote *= 24591257856;
-		sum += inte/pote;
-		
-		
+			sum *= 24591257856_mpz;
+			//Thread 1.3:
+			pote *= 24591257856_mpz;
+		sum += inte;
+
 		//Thread 2:
 		t = (t*t*2+1)/(t*4);
 	}
-	
+
 	return 0;
 }
